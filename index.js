bf5fca2d04fb7ea4b1323c8dd1981d30065ea6f5
GaziRiad <ouhibimustapha36@gmail.com>
2024-04-18 14:48:44 +0100

    Typescript basic types and union types

---
+++ index.js
@@ -0,0 +1,6 @@
+var userName;
+userName = "Max";
+var userAge;
+userAge = 22;
+var isValid = true;
+// string, number, boolean
---
+++ index.ts
@@ -0,0 +1,14 @@
+let userName: string;
+
+userName = "Max";
+
+let userAge: number;
+
+userAge = 22;
+
+let isValid = true;
+
+// string, number, boolean
+
+let userId: string | number = "abc1";
+userId = 123;
